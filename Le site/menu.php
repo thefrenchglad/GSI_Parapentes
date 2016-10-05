06e8e1536411232602824b8183fb2d42d2335917
Alexis-Petit <alexis-petit@live.fr>
2016-10-05 14:58:09 +0200

    update menu

---
+++ Le site/menu.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <link rel="stylesheet" href="css/style-menu.css">
+  <link rel="stylesheet" href="css/knacss.css">
+  <link href="https://fonts.googleapis.com/css?family=Pavanam" rel="stylesheet">
+
+  <title>Menu</title>
+</head>
+<body>
+
+  <header class="header-menu">
+
+    <div class="header-menu-logo">
+      <img src="img/logo.png" alt="">
+    </div>
+
+    <nav>
+      <a href="#">Tableau de bord</a>
+      <a href="#">Clients</a>
+      <a href="#">Locations</a>
+      <a href="#">Parapentes</a>
+      <a href="#">Contrôles techniques</a>
+      <a href="#">Parcours</a>
+    </nav>
+
+  </header>
+
+  <section class="tb-panel">
+    <div class="tb-item">
+      <header class="tb-item-header">
+        zbraloocnce 1R
+      </header>
+      <div class="tb-item-content">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe veniam nostrum obcaecati, magni odit, rerum quos id corporis cum ducimus labore dolor, vel dicta aperiam numquam nihil tenetur dolorum ex.
+      </div>
+    </div>
+    <div class="tb-item">
+      <header class="tb-item-header">
+        chabadabada 1
+      </header>
+      <div class="tb-item-content">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci et doloribus asperiores eveniet natus, sapiente. Deserunt hic, eius soluta cumque. Architecto obcaecati tempore ea totam! Fuga ipsam aliquid veniam beatae!
+      </div>
+    </div>
+    <div class="tb-item">
+      <header class="tb-item-header">
+        chabadabada 2
+      </header>
+      <div class="tb-item-content">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci et doloribus asperiores eveniet natus, sapiente. Deserunt hic, eius soluta cumque. Architecto obcaecati tempore ea totam! Fuga ipsam aliquid veniam beatae!
+
+      </div>
+    </div>
+    <div class="tb-item">
+      <header class="tb-item-header">
+        chabadabada 3
+      </header>
+      <div class="tb-item-content">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci et doloribus asperiores eveniet natus, sapiente. Deserunt hic, eius soluta cumque. Architecto obcaecati tempore ea totam! Fuga ipsam aliquid veniam beatae!
+
+      </div>
+    </div>
+  </section>
+
+</body>
+</html>
