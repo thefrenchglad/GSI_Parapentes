49f13379d0caee07c1c4f8159558079e559a0cbe
thefrenchglad <bonnot.kevin@gmail.com>
2016-10-08 18:27:17 +0200

    Formulaire passager
    Le formulaire de passager est maintenant fonctionnelle

--- Le site/post_passager.php
+++ Le site/post_passager.php
@@ -20,15 +20,17 @@
 
   try{
     $bdd = new PDO('mysql:host=localhost;dbname=gsi_parapentes_bdd;charset=utf8','root','');
-    echo 'Connexion réussi';
   }catch(Exception $e){
     die('Erreur : ' . $e->getMessage());
   }
 
   $date = $_POST['passager_annee']."-".$_POST['passager_mois']."-".$_POST['passager_jour'];
+  $nom = $_POST['passager_nom'];
+  $prenom = $_POST['passager_prenom'];
+  $poids = $_POST['passager_poids'];
 
-  $req1 = $bdd->prepare('insert into personne(nom,prenom,dateNaissance) values (:nom, :prenom, :dateNaissance)');
-  $req1 -> execute(array('nom' => $_POST['passager_nom'], 'prenom' => $_POST['passager_prenom'], 'dateNaissance' => $date));
+  $req1 = $bdd->prepare('insert into passager(nomPassager,prenomPassager,dateNaissancePassager,poidsPassager) values (:nom, :prenom, :dateNaissance, :poids)');
+  $req1 -> execute(array('nom' => $nom, 'prenom' => $prenom, 'dateNaissance' => $date, 'poids' => $poids));
 
 
 
