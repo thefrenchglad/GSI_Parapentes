8d091aa83330dba60f338cab53d62f50160eb649
Alexis-Petit <alexis-petit@live.fr>
2016-10-11 13:56:11 +0200

    final

---
+++ Le site/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <link rel="stylesheet" href="css/style-menu.css">
+  <link rel="stylesheet" href="css/knacss.css">
+  <link href="https://fonts.googleapis.com/css?family=Pavanam" rel="stylesheet">
+
+  <title>Menu</title>
+</head>
+
+  <header class="header-menu">
+
+    <div class="header-menu-logo">
+      <img src="img/logo.png" alt="">
+    </div>
+
+    <nav>
+      <a href="">Tableau de bord</a>
+      <a href="affichage_pilote.php">Pilotes</a>
+      <a href="affichage_passager.php">Passagers</a>
+      <a href="affichage_parapente.php">Parapentes</a>
+      <a href="affichage_vols.php">Vols</a>
+      <a href="affichage_reservation">Reservations</a>
+    </nav>
+
+  </header>
+
+</html>
